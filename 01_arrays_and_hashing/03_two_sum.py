df053039152cce329d1f4623b467db69d95282a2
Stefan Branković <stefan.brankovic2@gmail.com>
2026-03-30 17:35:05 +0200

    Create 03_two_sum.py

---
+++ 01_arrays_and_hashing/03_two_sum.py
@@ -0,0 +1,20 @@
+# Two sum
+# Difficulty: Easy
+# https://neetcode.io/problems/two-integer-sum/question
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # Hash map for storing differences and indexes
+        # Keys: difference values, Values: indexes in nums
+        # Time: O(n)  Space: O(n)
+        hash_map = {}
+        
+        for i in range(len(nums)):
+            diff = target - nums[i]
+
+            if diff in hash_map:
+                return [hash_map[diff], i]
+            else:
+                hash_map[nums[i]] = i
+
+        return []
