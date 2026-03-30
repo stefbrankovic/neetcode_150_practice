bb06fd02658b97a099b05c9b6dec02c523115a44
Stefan Branković <stefan.brankovic2@gmail.com>
2026-03-30 17:36:39 +0200

    Update 02_valid_anagram.py

--- 01_arrays_and_hashing/02_valid_anagram.py
+++ 01_arrays_and_hashing/02_valid_anagram.py
@@ -1,3 +1,6 @@
+# Valid Anagram
+# Difficulty: Easy
+# https://neetcode.io/problems/is-anagram/question?list=neetcode150
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         if len(s) != len(t):
